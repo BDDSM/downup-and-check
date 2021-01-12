a6f8f9306a589df770094e1514df55bc1a25087c
Al Iczin <al@iczin.guru>
2021-01-12 20:06:39 +0300

    вот так совсем правильней

--- prepare.yabr.bat
+++ prepare.yabr.bat
@@ -1,2 +1,4 @@
-opm build ./vendor/yabr/
-opm install yabr-0.6.2.ospx
+call opm build ./vendor/yabr/
+call opm install yabr-0.6.2.ospx
+call opm build ./vendor/yard
+call opm install yard-0.5.0.ospx
