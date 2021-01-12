557211e65b76d31b80e6e9097ea3d0c169fdcf9a
Al Iczin <al@iczin.guru>
2021-01-12 18:31:59 +0300

    Примерчик

--- downloader-accountint.bat
+++ downloader-accountint.bat
@@ -3,6 +3,4 @@ set LOGOS_CONFIG=logger.rootLogger=DEBUG
 chcp 866
 call users.bat
 
-rem --path ".\tmp\distr"
-
-yard releases --user "%USR%" --pwd "%PASS%" get --app-filter "Бухгалтерия предприятия, редакция 3.*" --version-start-date "01.12.2020" --path ".\tmp\distr" --extract --extract-path ".\tmp\tmplts"
+yard releases --user "%USR%" --pwd "%PASS%" get --app-filter "Бухгалтерия предприятия, редакция 3.*" --version-start-date "01.09.2020" --path ".\tmp\distr" --extract --extract-path ".\tmp\tmplts"
--- downloader-accountint.sh
+++ downloader-accountint.sh
@@ -1,8 +1,5 @@
-rem export LOGOS_CONFIG=logger.rootLogger=DEBUG
-export LOGOS_CONFIG=
+export LOGOS_CONFIG=logger.rootLogger=DEBUG
 
 source .env
-
-rem --path ".\tmp\distr"
 
 yard releases --user "$USR" --pwd "$PASS" get --app-filter "Бухгалтерия предприятия, редакция 3.*" --version-start-date "01.09.2020" --path ".\tmp\distr"
