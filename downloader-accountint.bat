030f22b78d73f740bc69a297778f159ae837e995
Al Iczin <al@iczin.guru>
2021-01-12 19:15:39 +0300

    Хотфикс путей

--- downloader-accountint.bat
+++ downloader-accountint.bat
@@ -3,4 +3,6 @@ set LOGOS_CONFIG=logger.rootLogger=DEBUG
 chcp 866
 call users.bat
 
-yard releases --user "%USR%" --pwd "%PASS%" get --app-filter "Бухгалтерия предприятия, редакция 3.*" --version-start-date "01.09.2020" --path ".\tmp\distr" --extract --extract-path ".\tmp\tmplts"
+set CURPATH=%~dp0
+
+yard releases --user "%USR%" --pwd "%PASS%" get --app-filter "Бухгалтерия предприятия, редакция 3.*" --version-start-date "01.09.2020" --path "%CURPATH%\tmp\distr" --extract --extract-path "%CURPATH%\tmp\tmplts"
